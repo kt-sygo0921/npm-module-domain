9aa8a149ef27f91be72b61c81de98ec5a0b6df52
onaka-taisuke <onaka-taisuke@dmm.com>
2020-02-17 16:21:29 +0900

    lib

--- lib/components/components.jsx
+++ lib/components/components.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import fetch from 'unfetch';
+import { Domain1 } from '@kt-sygo0921/npm-module-domain';
 const Components = () => {
     const [response, updateResponse] = React.useState(undefined);
     React.useEffect(() => {
@@ -10,6 +11,8 @@ const Components = () => {
         })();
     }, []);
     return (<>
+            <h1>表示てすと</h1>
+            <h2>非同期通信</h2>
             {response ? (<>
                     <p>非同期通信結果</p>
                     <ul>
@@ -19,6 +22,10 @@ const Components = () => {
                     </ul>
                     <p>画像テスト: <img src="https://placehold.jp/16x16.png"/></p>
                 </>) : (<p>loading...</p>)}
+            <div>
+                <h2>他のドメインのコンポーネントをimportして配置</h2>
+                <Domain1 />
+            </div>
         </>);
 };
 export { Components };
