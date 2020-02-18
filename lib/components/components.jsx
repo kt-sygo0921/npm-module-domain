042d515612602af10748911e7f0d738b8446475d
kato-syogo <kato-syogo@dmm.com>
2020-02-18 11:49:23 +0900

    バージョン検証用にreact-redux追加

--- lib/components/components.jsx
+++ lib/components/components.jsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
+import Modal from 'react-modal';
 const Components = () => {
-    return <p>コンポーネント1</p>;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
+    return (<>
+            <p>コンポーネント1</p>
+            <button onClick={openModal}>open modal</button>
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                <p>モーダルだよーーー</p>
+                <p onClick={closeModal} style={{ cursor: 'pointer' }}>クリックしたらモーダルが消えるよ</p>
+            </Modal>
+        </>);
 };
 export { Components };
